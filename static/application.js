29e2d7146a4037766e275ddfdcf66dbf76ba06c7
quirines <143832952+quirines@users.noreply.github.com>
2023-09-02 03:17:16 +0200

    Update application.js

--- static/application.js
+++ static/application.js
@@ -277,9 +277,9 @@ haste.prototype.configureButtons = function() {
       $where: $('#box2 .new'),
       label: 'New',
       shortcut: function(evt) {
-        return evt.ctrlKey && evt.keyCode === 78;
+        return evt.ctrlKey && evt.keyCode === 77;
       },
-      shortcutDescription: 'control + n',
+      shortcutDescription: 'control + m',
       action: function() {
         _this.newDocument(!_this.doc.key);
       }
